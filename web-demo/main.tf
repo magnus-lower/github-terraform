26c4716bc35babc1887f42f0e96f468de524c48d
Magnus Løwer <magnus.lower@gmail.com>
2024-10-01 08:53:09 +0200

    Added locals in the main.tf file

--- web-demo/main.tf
+++ web-demo/main.tf
@@ -1,3 +1,11 @@
+locals {
+  workspace_suffix = terraform.workspace == "default" ? "" : "${terraform.workspace}"
+
+  rg_name    = terraform.workspace == "default" ? "${var.rg_name}" : "${var.rg_name}-${local.workspace_suffix}"
+  sa_name    = terraform.workspace == "default" ? "${var.sa_name}" : "${var.sa_name}${local.workspace_suffix}"
+  web_suffix = "<h1>${terraform.workspace}</h1>"
+}
+
 resource "random_string" "random_string" {
   length  = 8
   special = false
@@ -6,13 +14,13 @@ resource "random_string" "random_string" {
 
 # Create Resource Group
 resource "azurerm_resource_group" "rg_web" {
-  name     = var.rg_name
+  name     = local.rg_name
   location = var.location
 }
 
 # Create Storage Account
 resource "azurerm_storage_account" "sa_web" {
-  name                     = "${lower(var.sa_name)}${random_string.random_string.result}"
+  name                     = "${lower(local.sa_name)}${random_string.random_string.result}"
   resource_group_name      = azurerm_resource_group.rg_web.name
   location                 = azurerm_resource_group.rg_web.location
   account_tier             = "Standard"
@@ -30,7 +38,7 @@ resource "azurerm_storage_blob" "index_html" {
   storage_container_name = "$web"
   type                   = "Block"
   content_type           = "text/html"
-  source_content         = var.source_content
+  source_content         = "${var.source_content}${local.web_suffix}"
 }
 
 output "primary_web_endpoint" {
