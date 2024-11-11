d6c78f304403e0ec5debce98d2c3669d41c690f5
Magnus Løwer <magnus.lower@gmail.com>
2024-11-11 09:07:49 +0100

    made changes

---
+++ module10/drift-detection/az-commands.sh
@@ -0,0 +1,2 @@
+az appservice plan show --name "maglowlab-dev-asp" --resource-group "maglowlab-dev-rg"
+az webapp config appsettings list --name "maglowlab-dev-webapp" --resource-group "maglowlab-dev-rg"
